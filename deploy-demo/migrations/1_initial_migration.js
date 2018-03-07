b3b15a9859bb278f699ee21030ec4cf551c16fdb
ywd <blockslimited@163.com>
2018-03-07 13:22:40 +0800

    contract deployment tests

--- deploy-demo/migrations/1_initial_migration.js
+++ deploy-demo/migrations/1_initial_migration.js
@@ -1,5 +1,16 @@
+const Web3 = require('web3');
+
+const TruffleConfig = require('../truffle.js');
+
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
+  const config = TruffleConfig.networks.development;
+  if (TruffleConfig.env === 'PROD') {
+    const web3 = new Web3(config.provider);
+    console.log('>> Unlocking account ' + config.coinbase);
+    web3.personal.unlockAccount(TruffleConfig.networks.coinbase, TruffleConfig.networks.password, 1000);
+  }
+  console.log('>> Deploying migration');
   deployer.deploy(Migrations);
 };
--- deploy-demo/truffle.js
+++ deploy-demo/truffle.js
@@ -1,11 +1,24 @@
+const Web3 = require('web3')
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  env: 'PROD',
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: "*",
+      provider: new Web3.providers.HttpProvider('https://mvchain.xyz'),
+      gas: 430000,
+      gasPrice: 2200000000,
+    },
+    coinbase: "0xe268d8c22739b9abdabdfb14763ff60d0de5e3ba",
+    password: "mvc123$%^"
+  },
+  // networks: {
+    // development: {
+      // host: "localhost",
+      // port: "8545",
+      // network_id: "*"
+    // }
+  // }
 };
