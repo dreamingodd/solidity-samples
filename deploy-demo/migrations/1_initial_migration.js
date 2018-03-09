68fdbcd9c4be94128f2d9d1d9c9b60ed187a784d
ywd <blockslimited@163.com>
2018-03-09 19:21:07 +0800

    truffle webpack tests

--- deploy-demo/migrations/1_initial_migration.js
+++ deploy-demo/migrations/1_initial_migration.js
@@ -6,11 +6,19 @@ var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
   const config = TruffleConfig.networks.development;
-  if (TruffleConfig.env === 'PROD') {
-    const web3 = new Web3(config.provider);
-    console.log('>> Unlocking account ' + config.coinbase);
-    web3.personal.unlockAccount(TruffleConfig.networks.coinbase, TruffleConfig.networks.password, 1000);
-  }
-  console.log('>> Deploying migration');
-  deployer.deploy(Migrations);
+  // if (TruffleConfig.env === 'PROD') {
+  //   const web3 = new Web3(config.provider);
+  //   console.log('>> Unlocking account ' + config.coinbase);
+  //   web3.personal.unlockAccount(TruffleConfig.networks.coinbase, TruffleConfig.networks.password, 1000);
+  // }
+  const web3 = new Web3(config.provider);
+  console.log(web3.eth.syncing);
+  // const config = TruffleConfig.networks.development;
+  // if (TruffleConfig.env === 'PROD') {
+  //   const web3 = new Web3(config.provider);
+  //   console.log('>> Unlocking account ' + config.coinbase);
+  //   web3.personal.unlockAccount(TruffleConfig.networks.coinbase, TruffleConfig.networks.password, 1000);
+  // }
+  // console.log('>> Deploying migration');
+  // deployer.deploy(Migrations);
 };
--- deploy-demo/test/test.js
+++ deploy-demo/test/test.js
@@ -2,15 +2,12 @@ const Web3 = require('web3');
 
 const TruffleConfig = require('../truffle.js');
 
-var Storage = artifacts.require("./Storage.sol");
-
-module.exports = function(deployer) {
+// module.exports = function(deployer) {
   // const config = TruffleConfig.networks.development;
   // if (TruffleConfig.env === 'PROD') {
   //   const web3 = new Web3(config.provider);
   //   console.log('>> Unlocking account ' + config.coinbase);
   //   web3.personal.unlockAccount(TruffleConfig.networks.coinbase, TruffleConfig.networks.password, 1000);
   // }
-  // console.log('>> Deploying migration');
-  // deployer.deploy(Storage);
-};
+  console.log("aaaa")
+// }
