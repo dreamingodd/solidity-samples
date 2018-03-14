0ed5f11ad5adb64442d3f9447f9090d6c2d27276
YWD@MVC <memorywilllast@gmail.com>
2018-03-14 20:05:31 +0800

    transfer tests

--- truffle-webpack-demo-b/contracts/HumanStandardToken.sol
+++ truffle-webpack-demo-b/contracts/HumanStandardToken.sol
@@ -19,12 +19,17 @@ contract HumanStandardToken is StandardToken {
     They allow one to customise the token contract & in no way influences the core functionality.
     Some wallets/interfaces might not even bother to look at this information.
     */
-    string public name;                   //fancy name: eg Simon Bucks
-    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
-    string public symbol;                 //An identifier: eg SBX
+    string public name = 'Ye, Wenda';                   //fancy name: eg Simon Bucks
+    uint8 public decimals = 2;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol = 'YWD';                 //An identifier: eg SBX
     string public version = 'H0.1';       //human 0.1 standard. Just an arbitrary versioning scheme.
 
-    function HumanStandardToken(
+    function HumanStandardToken() {
+      balances[msg.sender] = 1000;               // Give the creator all initial tokens
+      totalSupply = 1000;
+    }
+
+    /* function HumanStandardToken(
         uint256 _initialAmount,
         string _tokenName,
         uint8 _decimalUnits,
@@ -35,7 +40,7 @@ contract HumanStandardToken is StandardToken {
         name = _tokenName;                                   // Set the name for display purposes
         decimals = _decimalUnits;                            // Amount of decimals for display purposes
         symbol = _tokenSymbol;                               // Set the symbol for display purposes
-    }
+    } */
 
     /* Approves and then calls the receiving contract */
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
